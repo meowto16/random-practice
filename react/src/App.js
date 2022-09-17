d21832c0476d715deea87c7eb1033621416da863
Maxim Kirshin <meowto16@gmail.com>
2022-09-17 12:42:18 +0500

    [react-experiments]: Simple form example

--- react/src/App.js
+++ react/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from 'react-hook-form'
+
+import './App.css'
 
 function App() {
+  const { register, handleSubmit, formState } = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data)
+    console.log(formState)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="First name" {...register('First name', { required: true, maxLength: 80 })} />
+        <input type="text" placeholder="Last name" {...register('Last name', { required: true, maxLength: 100 })} />
+        <input type="text" placeholder="Email" {...register('Email', { required: true, pattern: /^\S+@\S+$/i })} />
+        <input type="tel" placeholder="Mobile number" {...register('Mobile number', {
+          required: true,
+          minLength: 6,
+          maxLength: 12
+        })} />
+        <select {...register('Title', { required: true })}>
+          <option value="Mr">Mr</option>
+          <option value="Mrs">Mrs</option>
+          <option value="Miss">Miss</option>
+          <option value="Dr">Dr</option>
+        </select>
+
+        <input {...register('Developer', { required: true })} type="radio" value="Yes"/>
+        <input {...register('Developer', { required: true })} type="radio" value="No"/>
+
+        <input type="submit"/>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
